5e69e3c5f5f8ecac5e4586bcefc7979ee049ce94
Jun Hong <ong.junhong888@gmail.com>
2025-11-08 18:32:30 -0500

    removed more ig code

--- backend/src/metrics/admin.py
+++ backend/src/metrics/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-
-from metrics.models import PostMetrics, IGMetrics
+from metrics.models import PostMetrics
 
 # Register your models here.
 class XAdmin(admin.ModelAdmin):
     list_display = ('pk', 'likes', 'retweets')
 
-class IGAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'likes', 'shares')
-
 admin.site.register(PostMetrics, XAdmin)
-admin.site.register(IGMetrics, IGAdmin)
