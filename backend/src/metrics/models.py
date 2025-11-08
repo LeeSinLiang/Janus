f21cc04835a0e56ed1d29ccf149bec91bb36c81c
Jun Hong <ong.junhong888@gmail.com>
2025-11-08 00:41:22 -0500

    ..

--- backend/src/metrics/models.py
+++ backend/src/metrics/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from agents.models import Post
 
 # Create your models here.
 class NodeMetrics(models.Model):
     node_id = models.BigAutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     likes = models.IntegerField()
     impressions = models.IntegerField()
     retweets = models.IntegerField()
