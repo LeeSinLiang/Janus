bb3e3b3c85187fb1a45c987dc9b5dfc3202e6383
Jun Hong <ong.junhong888@gmail.com>
2025-11-07 22:44:35 -0500

    added display

--- backend/src/metrics/views.py
+++ backend/src/metrics/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import NodeMetrics
 
 # Create your views here.
-def metrics(request):
-    metrics = Members.objects.all().values()
-    template = loader.get_template('memberList.html')
+def nodeMetricsDisplay(request, nodeId):
+    node = NodeMetrics.objects.get(node_id=nodeId)
+    template = loader.get_template('metricsDisplay.html')
     context = {
-        'members': members,
+        'node_id': node.node_id,
+        'likes': node.likes,
+        'impressions': node.impressions,
+        'retweets': node.retweets,
     }
     return HttpResponse(template.render(context, request))
